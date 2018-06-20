c0cc4a55fc2d83891eb5cb701e1db62d1cffafbd
deanwebbdeveloper <dean@deanwebbdeveloper.com>
2018-06-20 22:00:23 +0100

    Chapter 1
    Chapter 1 completed, interactive section completed on site

---
+++ 1 - Introduction/1.1 - What is C#/TryCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TryCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a message");
+            Console.WriteLine("You said \"" + Console.ReadLine() + "\"");
+        }
+    }
+}
